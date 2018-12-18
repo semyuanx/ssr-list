34986a3bafb5c20608c7ac70c262a7d0c4a7123c
Viktor <victor318x@gmail.com>
2018-12-18 18:14:08 +0800

    fix: 筛选器结果页的筛选按钮

--- src/components/mobile-filter/FilterList.vue
+++ src/components/mobile-filter/FilterList.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="filter-list-container">
     <section class="filter-selected-container">
-      <filter-button class="filter-button">收益率 >10%</filter-button>
-      <filter-button class="filter-button">最大回测 >10%</filter-button>
+      <div>
+        <filter-button class="filter-button">收益率 >10%</filter-button>
+        <filter-button class="filter-button">最大回测 >10%</filter-button>
+      </div>
+      <router-link
+        to="/filter"
+        class="filter-link"
+      >
+        <span>筛选</span>
+      </router-link>
     </section>
     <section class="filter-result-container">
       <v2-table
@@ -119,11 +127,17 @@ export default class FilterList extends Vue {
 
 
 <style lang="less" scoped>
+@theme-color: #ff6200;
 .filter-list-container {
   font-family: PingFang-SC-Medium;
   color: #333;
   font-weight: 500;
   background-color: #fff;
+  .filter-link {
+    text-decoration: none;
+    color: @theme-color;
+    font-size: 14px;
+  }
 }
 .filter-result-container {
   padding: 0 15px;
@@ -133,8 +147,11 @@ export default class FilterList extends Vue {
   }
 }
 .filter-selected-container {
-  border-bottom: 1px solid #EBEEF5;
+  border-bottom: 1px solid #ebeef5;
   padding: 0 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   .filter-button {
     margin-right: 10px;
     margin-bottom: 6px;
