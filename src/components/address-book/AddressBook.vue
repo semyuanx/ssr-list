4cd7c070c9cc5090bdd05d7dd689ca7551566ee5
Viktor <victor318x@gmail.com>
2018-12-19 19:36:01 +0800

    feat: 复选框组插件

--- src/components/address-book/AddressBook.vue
+++ src/components/address-book/AddressBook.vue
@@ -3,36 +3,35 @@
     <div class="page page-inner">
       <search-input></search-input>
       <div class="content">
-        <div class="list-block contacts-block">
-          <div class="list-group">
-            <ul id="A">
-              <li
-                class="list-group-title"
-                v-show="hasTitle"
-              >A</li>
-              <li
-                v-for="(item,index) in 30"
-                :key="index"
-              >
-                <div class="item-content">
-                  <div class="item-title">
-                    <check-box
-                      shape="circle"
-                      v-model="checkbox"
-                    >
-                      <span class="main-title">USD/JPY</span>
-                      <span class="sub-title">美元/日元</span>
-                    </check-box>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <check-box-group v-model="checkbox">
+          <div class="list-block contacts-block">
+            <div class="list-group">
+              <ul id="A">
+                <li class="list-group-title" v-show="hasTitle">A</li>
+                <li
+                  v-for="(item,index) in 30"
+                  :key="index"
+                >
+                  <div class="item-content">
+                    <div class="item-title">
+                      <check-box
+                        shape="circle"
+                        :val="index"
+                      >
+                        <span class="main-title">USD/JPY</span>
+                        <span class="sub-title">美元/日元</span>
+                      </check-box>
 
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </check-box-group>
       </div>
     </div>
-    <book-order />
+    <book-order/>
   </div>
 </template>
 
@@ -43,6 +42,7 @@ import {
 import SearchInput from './SearchInput.vue';
 import BookOrder from './BookOrder.vue';
 import CheckBox from '@/components/check-box/CheckBox.vue';
+import CheckBoxGroup from '@/components/check-box/CheckBoxGroup.vue';
 
 import zhCN from '@/i18n/zh-CN/components/filter-popover/FilterTag';
 import zhTW from '@/i18n/zh-TW/components/filter-popover/FilterTag';
@@ -60,6 +60,7 @@ import enUS from '@/i18n/en-US/components/filter-popover/FilterTag';
     SearchInput,
     BookOrder,
     CheckBox,
+    CheckBoxGroup,
   },
 })
 export default class AddressBook extends Vue {
@@ -68,7 +69,7 @@ export default class AddressBook extends Vue {
   })
   hasTitle!: boolean;
 
-  checkbox: boolean = false;
+  checkbox: number[] = [1, 2, 3];
 }
 </script>
 
