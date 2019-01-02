e9c06718a8f62c65ed3358f5c378561c55b7f373
hellokitty <wujian@followme-inc.com>
2019-01-02 18:37:20 +0800

    feat: fill list data in homepage

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,5 @@
 import numeral from 'numeral';
 
-
 export const numberFormat = function numberFormat(val: string|number, format: number = 2) {
   if (!val) {
     return 0;
@@ -26,6 +25,29 @@ export const percentFormat = function percentFormat(val: string|number, format: 
   type = `0.${type}%`;
   const transformed = v.format(type);
   return transformed && transformed.includes('NaN') ? '0%' : transformed;
+};
+
+export const propFormat = function propFormat(val: string | number, prop: string) {
+  const percentKeys = ['ROI'];
+  const dotKeys = [
+    'Equity', 'DealAmount', 'MaxRetracement',
+    'TotalStandardlots', 'TotalSelfStandardlots',
+    'TotalFollowStandardlots', 'TotalPips',
+    'TotalSelfPips', 'TotalFollowPips',
+    'Score', '',
+  ];
+  const moneyKeys = [
+    'Money', 'TotalMoney', 'TotalSelfMoney',
+    'TotalFollowMoney', 'FollowMoney',
+  ];
+  if (percentKeys.includes(prop)) {
+    return percentFormat(val);
+  } if (dotKeys.includes(prop)) {
+    return numberFormat(val);
+  } if (moneyKeys.includes(prop)) {
+    return val;
+  }
+  return val;
 };
 
 export const none = () => {};
