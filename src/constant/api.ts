1463a72e169a77b6f758e31f181058f5e25850a8
hellokitty <wujian@followme-inc.com>
2019-01-10 10:44:10 +0800

    fix: fix base api errror

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,7 +1,7 @@
 
 export const PAGE_SIZE: number = 1;
-export const REQUEST_BASE_URL: string = process.env.VUE_APP_FM_API || '';
-export const REQUEST_AUTH_URL: string = process.env.VUE_APP_FM_AUTH || '';
+export const REQUEST_BASE_URL: string = window.FM_API || process.env.VUE_APP_FM_API || '';
+export const REQUEST_AUTH_URL: string = window.FM_AUTH || process.env.VUE_APP_FM_AUTH || '';
 export const API_PREFIX_V1: string = `${REQUEST_BASE_URL}/api/v1`;
 export const API_PREFIX_V2: string = `${REQUEST_BASE_URL}/api/v2`;
 export const API_PREFIX_V3: string = `${REQUEST_BASE_URL}/api/v3`;
---
+++ src/views/follow/ListMobile.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="list-container">
+  <div class="list-body">
+    <MobileFilterList :loading="dateIsLoading" @subscribe="subscribe" :data="data" @sortChange="sortChange"/>
+  </div>
+</div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import SvgIcon from '@/components/svg/index.ts';
+import MobileFilterList from '@/components/mobile-filter/FilterList.vue';
+import { toLoginPage, toSubscribePage } from '@/utils/native';
+
+const RankStore = namespace('RankStore');
+
+@Component({
+  components: {
+    SvgIcon,
+    MobileFilterList,
+  },
+})
+export default class List extends Vue {
+  @RankStore.State
+  rankListLoading: any;
+
+  @RankStore.Action
+  getLoginStatus: any;
+
+  @Prop({
+    type: Array,
+    default: () => [],
+  })
+  data: any;
+
+  public get dateIsLoading() {
+    return this.rankListLoading;
+  }
+
+  async subscribe(data: any) {
+    const res: any = await this.getLoginStatus();
+    if (res.isLogin) {
+      toSubscribePage({
+        userId: data.item.UserID,
+        accountIndex: data.item.AccountIndex,
+        nickName: data.item.NickName,
+      });
+    } else {
+      toLoginPage();
+    }
+  }
+
+  mounted() {
+    // setTimeout(() => this.isLoading = false, 5000);
+  }
+
+  sortChange({ prop, order }:any) {
+    this.$emit('sortChange', { prop, order });
+  }
+}
+</script>
+<style lang="less" scoped>
+.list-container {
+
+}
+
+</style>
